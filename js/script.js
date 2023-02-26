90d380c0a36352157bf2a93deb1671ab8aa941da
Daniel Ezekiel <dbezekiel77@gmail.com>
2023-02-26 21:23:58 +0100

    feat: styled and implemented mobile responsive navbar and hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const hamburger = document.querySelector('.btn--hamburger');
+const mobileNav = document.querySelector('.header__nav');
+
+hamburger.addEventListener('click', function () {
+  hamburger.classList.toggle('active');
+  mobileNav.classList.toggle('active');
+});
